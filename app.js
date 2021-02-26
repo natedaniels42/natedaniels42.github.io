555888bd095f28ba2aaad8e8cfd0630834ff6fb5
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-02-26 15:23:45 -0600

    more styling

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const homeButton = document.getElementById('home-button');
 const banner = document.getElementById('banner');
 const projectButton = document.getElementById('projects-button');
 const projects = document.getElementById('projects');
+const socialButton = document.getElementById('social-button');
+const social = document.getElementById('social');
 let currentSection = banner;
 
 projectButton.addEventListener('click', () => {
     projects.style.opacity = '1';
     projects.style.transition = 'opacity 2s';
     currentSection.style.opacity = '0';
-    currentSection.style.trnasition = 'opacity 2s';
+    currentSection.style.transition = 'opacity 2s';
     currentSection = projects;
 })
 
@@ -16,6 +18,14 @@ homeButton.addEventListener('click', () => {
     banner.style.opacity = '1';
     banner.style.transition = 'opacity 2s';
     currentSection.style.opacity = '0';
-    currentSection.style.trnasition = 'opacity 2s';
+    currentSection.style.transition = 'opacity 2s';
     currentSection = banner;
+})
+
+socialButton.addEventListener('click', () => {
+    social.style.opacity = '1';
+    social.style.transition = 'opacity 2s';
+    currentSection.style.opacity = '0';
+    currentSection.style.transition = 'opacity 2s';
+    currentSection = social;
 })
